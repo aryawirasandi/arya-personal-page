1d373d9e2dad1f755e6d11a54344cf3ddddfad2c
aryawirasandi <aryawirasandi7153@gmail.com>
2024-08-25 21:21:17 +0700

    add email submit feature

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 	import { slide } from "svelte/transition";
+    import type { ActionData }  from "./$types"
+    import Error from "$lib/components/Error.svelte"
+    import Success from "$lib/components/Success.svelte";
+    export let form:ActionData
     let showAnimate = false;
     onMount(() => {
         showAnimate = true;
     })
 </script>
+<svelte:head>
+	<title>Contact Me </title>
+    <meta name="description" content="to find out more, you can contact me!">
+</svelte:head>
 <div class="p-4 min-h-screen justify-center flex flex-col">
     <h1 class="text-white text-4xl">
         Contact Me!
@@ -21,6 +29,40 @@
             <a href="https://wa.me/+6282218341631?text=Hello%20Arya">WhatsApp (+6282281341631)</a>
         </li>
     </ul>
+    <div class="md:w-1/2 lg:w-1/2 mt-4">
+        {#if typeof form?.success !== "undefined" }
+           {#if form.success}
+            <Success>
+                {form.message}
+            </Success>
+            {:else}
+            <Error>
+                {form.message}
+            </Error>
+           {/if}
+        {/if}
+        <form class="grid grid-cols-1" method="POST" action="/contact">
+            <label class="text-white flex flex-col">
+                <span class="mb-3">
+                    email
+                </span>
+                <input class="text-black rounded-sm px-3 outline-none py-2 font-sans" placeholder="please enter your email" name="email" type="text">
+            </label>
+            <label class="text-white flex flex-col mt-3">
+                <span class="mb-3">
+                    subject
+                </span>
+                <input class="text-black rounded-sm px-3 outline-none py-2 font-sans" placeholder="please enter your subject" name="subject" type="text">
+            </label>
+            <label class="text-white flex flex-col mt-4 font-sans">
+                <span class="mb-3">
+                    Description
+                </span>
+                <textarea name="description" class="rounded-sm outline-none text-black px-3 py-2" placeholder="please enter your description"></textarea>
+            </label>
+            <button class="rounded-sm bg-green-400 text-white mt-4 p-3">Send Mail</button>
+        </form>
+    </div>
 </div>
 {#if showAnimate}
     <div transition:slide={{ axis: "y"}}>
