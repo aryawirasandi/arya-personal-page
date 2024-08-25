ba5878c8f71109f1868f71c149d3302aa9b246d7
aryawirasandi <aryawirasandi7153@gmail.com>
2024-08-25 21:20:10 +0700

    remove "-" character

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -14,5 +14,5 @@
     {#if showAnimate}
         <h1 transition:slide={{ axis: "x"}} class="text-4xl gap-5 my-4">Arya Wirasandi</h1>
     {/if}
-    <Description classNameRoot="md:w-1/2" description="Hi, I'm Arya, a Software Engineer specializing in frontend development. I've built some amazing projects—explore more in my project section!"/>
+    <Description classNameRoot="md:w-1/2" description="Hi, I'm Arya, a Software Engineer specializing in frontend development. I've built some amazing projects, explore more in my project section!"/>
 </div>
